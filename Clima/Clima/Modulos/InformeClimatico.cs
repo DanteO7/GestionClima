f14c2c2cab6161a01fbf85d9922c3e3331b1899f
DanteO7 <166159559+DanteO7@users.noreply.github.com>
2024-08-25 18:27:30 -0300

    Add files via upload

---
+++ Clima/Clima/Modulos/InformeClimatico.cs
@@ -0,0 +1,20 @@
+﻿namespace GestionClimatica.Modulos
+{
+    public class InformeClimatico
+    {
+        public DateTime Fecha = DateTime.Now;
+        public string Resumen {  get; set; }
+        public Ubicacion Ubicacion { get; set; }
+        public InformeClimatico(string resumen, Ubicacion ubicacion)
+        {
+            Resumen = resumen;
+            Ubicacion = ubicacion;
+        }
+        public void GenerarInforme()
+        {
+            Console.WriteLine($"\nFecha {Fecha}");
+            Ubicacion.MostrarDetalles();
+            Console.WriteLine($"Resumen: {Resumen}");
+        }
+    }
+}
---
+++ Clima/Clima/Modulos/RedClimatica.cs
@@ -0,0 +1,32 @@
+﻿namespace GestionClimatica.Modulos
+{
+    public class RedClimatica
+    {
+        public string Jefe { get; set; }
+        private List<Estacion> _estaciones;
+
+        public RedClimatica(string jefe, List<Estacion> estaciones)
+        {
+            Jefe = jefe;
+            if (estaciones.Count < 2)
+            {
+                throw new Exception("Cantidad de estaciones no válidas. Se necesitan al menos 2");
+            }
+            _estaciones = estaciones;
+        }
+
+        public void AgregarEstacion(Estacion estacion)
+        {
+            _estaciones.Add(estacion);
+        }
+
+        public void MostrarDetalle()
+        {
+            Console.WriteLine($"Jefe De la red: {Jefe}");
+            foreach (var estacion in _estaciones)
+            {
+                estacion.MostrarDetalle();
+            }
+        }
+    }
+}
