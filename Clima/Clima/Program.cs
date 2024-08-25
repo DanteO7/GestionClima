f68524937fe0877bb9e74471ff061e56d9183451
DanteO7 <166159559+DanteO7@users.noreply.github.com>
2024-08-25 18:28:15 -0300

    Program.cs

--- Clima/Clima/Program.cs
+++ Clima/Clima/Program.cs
@@ -1,14 +1,28 @@
-﻿using GestionClimatica.Modulos;
+using GestionClimatica.Modulos;
 
 class Program
 {
     static void Main()
     {
-        Clima climita = new Clima(10, "Nublado");
-        Estacion estacioncita = new Estacion(1, "Calle Falsa 123");
-        Ubicacion ubicacioncita = new Ubicacion("Villa Constitucion", "Argentina", climita, estacioncita);
-        Meteorologo meteorologito = new Meteorologo("Juancito", estacioncita);
-        
-        meteorologito.ReportarCondicion(ubicacioncita);
+        Clima unClima = new Clima(11, "Nublado");
+
+        Ubicacion unaUbicacion1 = new Ubicacion("San Nicho", "Argentina", unClima);
+        Ubicacion unaUbicacion2 = new Ubicacion("Villa Constitucion", "Argentina", unClima);
+        Ubicacion unaUbicacion3 = new Ubicacion("Rosario", "Argentina", unClima);
+
+        Meteorologo meteorologo1 = new Meteorologo("Pepito", "Agricola");
+        Meteorologo meteorologo2 = new Meteorologo("Juancito", "Normal");
+        Meteorologo meteorologo3 = new Meteorologo("Pedrito", "Normal");
+
+        Estacion estacion1 = new Estacion(unaUbicacion1, meteorologo1);
+        Estacion estacion2 = new Estacion(unaUbicacion2, meteorologo2);
+        Estacion estacion3 = new Estacion(unaUbicacion3, meteorologo3);
+
+        RedClimatica unaRedClimatica = new RedClimatica("Juancito", new List<Estacion> { estacion1, estacion2, estacion3 });
+
+        unaRedClimatica.MostrarDetalle();
+
+        InformeClimatico unInforme = new InformeClimatico("Hoy hizo calor", unaUbicacion3);
+        unInforme.GenerarInforme();
     }
 }
